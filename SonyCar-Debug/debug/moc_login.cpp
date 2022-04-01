526806012cf6534db437096e69596931f20ce323
pengdahong1225 <2102533303@qq.com>
2022-04-01 11:14:25 +0800

    2020/4/1

--- SonyCar-Debug/debug/moc_login.cpp
+++ SonyCar-Debug/debug/moc_login.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../login.h"
+#include "../../../../../Desktop/Sony-Host-computer-developer/Sony-Host-computer-developer/login.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
