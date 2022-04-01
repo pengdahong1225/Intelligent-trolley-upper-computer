53f628b365ad367f6c671a904a06928a4a7f71a4
pengdahong1225 <2102533303@qq.com>
2022-04-01 11:25:23 +0800

    2022/4/1

--- SonyCar-Debug/debug/moc_sock.cpp
+++ SonyCar-Debug/debug/moc_sock.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../../Desktop/Sony-Host-computer-developer/Sony-Host-computer-developer/sock.h"
+#include "../../sock.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Sock_t {
-    QByteArrayData data[8];
-    char stringdata0[79];
+    QByteArrayData data[10];
+    char stringdata0[117];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,11 +38,14 @@ QT_MOC_LITERAL(3, 17, 9), // "receiveOK"
 QT_MOC_LITERAL(4, 27, 9), // "sock_disc"
 QT_MOC_LITERAL(5, 37, 18), // "incomingConnection"
 QT_MOC_LITERAL(6, 56, 7), // "qintptr"
-QT_MOC_LITERAL(7, 64, 14) // "receiveMessage"
+QT_MOC_LITERAL(7, 64, 14), // "receiveMessage"
+QT_MOC_LITERAL(8, 79, 8), // "MSGError"
+QT_MOC_LITERAL(9, 88, 28) // "QAbstractSocket::SocketError"
 
     },
     "Sock\0NewConnect\0\0receiveOK\0sock_disc\0"
-    "incomingConnection\0qintptr\0receiveMessage"
+    "incomingConnection\0qintptr\0receiveMessage\0"
+    "MSGError\0QAbstractSocket::SocketError"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +55,7 @@ static const uint qt_meta_data_Sock[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,13 +63,14 @@ static const uint qt_meta_data_Sock[] = {
        3,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    2,   39,    2, 0x06 /* Public */,
-       3,    1,   44,    2, 0x06 /* Public */,
-       4,    0,   47,    2, 0x06 /* Public */,
+       1,    2,   44,    2, 0x06 /* Public */,
+       3,    1,   49,    2, 0x06 /* Public */,
+       4,    0,   52,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    1,   48,    2, 0x0a /* Public */,
-       7,    0,   51,    2, 0x0a /* Public */,
+       5,    1,   53,    2, 0x0a /* Public */,
+       7,    0,   56,    2, 0x0a /* Public */,
+       8,    1,   57,    2, 0x0a /* Public */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString, QMetaType::UShort,    2,    2,
@@ -76,6 +80,7 @@ static const uint qt_meta_data_Sock[] = {
  // slots: parameters
     QMetaType::Void, 0x80000000 | 6,    2,
     QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,    2,
 
        0        // eod
 };
@@ -91,7 +96,19 @@ void Sock::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         case 2: _t->sock_disc(); break;
         case 3: _t->incomingConnection((*reinterpret_cast< qintptr(*)>(_a[1]))); break;
         case 4: _t->receiveMessage(); break;
+        case 5: _t->MSGError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 5:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
@@ -148,13 +165,13 @@ int Sock::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
     }
     return _id;
 }
